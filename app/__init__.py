fe2ce629b1f56a9a76b2ce8076bf283d3494cefa
unknown <jordan.barrett.jm@gmail.com>
2022-03-06 20:59:01 -0500

    added db config

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
