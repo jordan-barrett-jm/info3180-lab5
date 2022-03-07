2c61c80f14672192358ba7588e257d75b62daed4
unknown <jordan.barrett.jm@gmail.com>
2022-03-06 21:20:54 -0500

    completed implementation of login function

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -34,20 +35,24 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
-
+            req_username = form.username.data
+            req_password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
-
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            user = UserProfile.query.filter_by(username=req_username).first()
+            if user and check_password_hash(user.password, req_password):
+                # get user id, load into session
+                login_user(user)
+                # remember to flash a message to the user
+                flash("User authenticated.")
+                return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash("Incorrect credentials entered.")
     return render_template("login.html", form=form)
 
 
